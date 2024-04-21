766f2b317b6ebdb9f37351769c3230555fd1155b
wl4dig3 <wladige@gmail.com>
2024-04-21 19:02:35 -0400

    chore: refactorizando para usar api Pokemon

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -2,20 +2,23 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 function Details() {
+    // const [loading, setLoading] = useEffect(true):
     const [personaje, setPersonaje] = useState()
     const {id} = useParams();
     useEffect( () => {
-        fetch(`https://rickandmortyapi.com/api/character/${id}`)
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
         .then(resp => resp.json())
         .then(data => setPersonaje(data))
-
+        // setLoading(false)
     },[id])
   return (
     <div>
       {personaje !== undefined ? (
         <>
           <h1>{personaje.name}</h1>
-          <img src={personaje.image} alt={personaje.name} />
+          {/* <img src={personaje.image} alt={personaje.name} /> */}
+          {/* <p>Estatus: {personaje.status}</p> */}
+          {/* <p>Especie: {personaje.species}</p> */}
         </>
       ) : (
         <h2>No existe ese personaje</h2>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,27 @@ import CardPersonajes from '../components/CardPersonajes';
 
 function Home() {
     const [personajes, setPersonajes] = useState([]);
-    const BASE_URL = 'https://rickandmortyapi.com/api/character'
+    const [offset, setOffset] = useState(0);
+
+    const BASE_URL = 'https://pokeapi.co/api/v2/';
+    const getAllPokemon = async (limit = 50) => {
+        const resp = await fetch(`${BASE_URL}pokemon?limit=${limit}&offset=${offset}`)
+        const data = await resp.json();
+        const promises = data.results.map(async(pokemon)=> {
+            const resp = await fetch(pokemon.url);
+            const data = await resp.json();
+            return data
+        });
+        const results = await Promise.all(promises);
+        setPersonajes(results);
+    }
     useEffect( () => {
-        fetch(BASE_URL)
-        .then(resp => resp.json())
-        .then(data => setPersonajes(data.results))
+        getAllPokemon()
     }, [])
   return (
     <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', flexWrap: ' wrap' }}>
         {
-            personajes.map( p => <CardPersonajes key={p.id} {...p} />)
+            personajes.map( p => <CardPersonajes key={p.name} {...p} />)
         }
     </div>
   )
